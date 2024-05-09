e88b9b44a412c6a9314ca6e102f38f3b8a4e72db
tos1049 <tos@mtf.biglobe.ne.jp>
2024-05-09 16:04:56 +0900

    [ADMIN] wintestをリポジトリ登録
    ウィンドウ機能をもう少し端的にテストする環境としてリポジトリ登録しておく。

---
+++ wintest/com_spec.c
@@ -0,0 +1,126 @@
+/*
+ *****************************************************************************
+ *
+ * comモジュール  個別I/F定義  by TOS
+ *
+ *   comモジュールの処理の中でも、アプリごとに個別の定義が必要なものを、
+ *   このソースに追加する。もちろんさらに別のソースとしても問題はない。
+ *   本ファイル内の関数プロトタイプ等は com_spec.h に記載すること。
+ *
+ *   com_spec.h自体は com_if.hでインクルードしているため、インクルード文を
+ *   さらに記述する必要はない。com_if.hは全ソースで最初にインクルードすることが
+ *   前提となっているため、どのソースでも本ソースのI/Fが使えることになる。
+ *
+ *****************************************************************************
+ */
+
+#include "com_if.h"
+
+/* サンプル個別処理 *********************************************************/
+// プロトタイプ宣言はいずれも com_spec.h
+
+static BOOL checkEndian( void )
+{
+    // この判定方法は C言語FAQ 20.9 に記載されている
+    int  x = 1;
+    if( *(char*)&x == 1 ) {return false;}  // リトルエンディアン
+    return true;  // ビッグエンディアン
+}
+
+BOOL com_isBigEndian( void )
+{
+    return checkEndian();
+}
+
+BOOL com_isLittleEndian( void )
+{
+    return !checkEndian();
+}
+
+BOOL com_is32bitOS( void )
+{
+    return (sizeof(long) == sizeof(int));
+}
+
+BOOL com_is64bitOS( void )
+{
+    return (sizeof(long) != sizeof(int));
+}
+
+COM_OS_TYPE_t com_getEnvName( void )
+{
+#if defined __linux__
+    return COM_OS_LINUX;
+#elif defined  __CYGWIN__
+    return COM_OS_CYGWIN;
+#else
+    return COM_OS_NOT_SUPPORTED;
+#endif
+}
+
+
+/* 個別エラー定義 ***********************************************************/
+
+// com_spec.hのエラー定義を変更したら、こちらも連動で変更する
+static com_dbgErrName_t  gErrorNameSpec[] = {
+    { COM_ERR_DUMMY,        "COM_ERR_DUMMY" },  // これは自由に削除可能
+
+    { COM_ERR_END, "" }  // 最後は必ずこれで(絶対削除しないこと)
+};
+
+void com_registerErrorCodeSpec( void )
+{
+    com_registerErrorCode( gErrorNameSpec );   // 本行は削除禁止
+}
+
+
+
+/* 個別初期化処理 ***********************************************************/
+
+// com_spec.hのプロトタイプ宣言に付したI/F説明をよく読んでから変更すること
+void com_initializeSpec( void )
+{
+}
+
+
+
+/* 個別終了処理 *************************************************************/
+
+void com_finalizeSpec( void )
+{
+}
+
+
+
+/***************************************************************************
+ ******************* これ以降のソースは変更しないこと **********************
+ ***************************************************************************/
+
+// 以下の関数群のプロトタイプ宣言は com_if.h
+
+/* makefile のコンパイルオプション APLNAMEで指定された文字列 */
+static char  gAPLNAME[] =
+#ifdef APLNAME    // "アプリ名" の形式で宣言すること
+    APLNAME;
+#else
+    "";    // 定義がないときは空文字とする
+#endif
+
+const char *com_getAplName( void )
+{
+    return gAPLNAME;
+}
+
+/* makefile のコンパイルオプション VERSIONで指定された文字列 */
+static char  gVERSION[] =
+#ifdef VERSION    // "バージョン" の形式で宣言すること
+    VERSION;
+#else
+    "0";   // 定義がないときは "0" とする
+#endif
+
+const char *com_getVersion( void )
+{
+    return gVERSION;
+}
+
---
+++ wintest/main.c
@@ -0,0 +1,239 @@
+/*
+ * main() ひな形
+ *   toscomを使う際の最も基本的なひな形となるコード。
+ *   初期化の処理は、基本的にこのパターンにしたほうが良い。
+ *   実行しても何もせず終了となる。
+ */
+
+#include "com_if.h"                 // toscom基本機能
+#include "com_extra.h"
+#include "com_window.h"
+
+/* 独自処理があれば記述 ******************************************************/
+
+enum { WIN_MAX = 5 };
+static com_winId_t gWinId[WIN_MAX];
+static long gCurWin = 0;
+
+static BOOL gLoop = true;
+
+static void switchWindow( com_winId_t iId )
+{
+    gCurWin = (iId + 1) % (WIN_MAX + 1);
+}
+
+static void revertWindow( com_winId_t iId )
+{
+    gCurWin = iId - 1;
+    if( gCurWin < 0 ) { gCurWin = WIN_MAX; }
+}
+
+static void moveCursor( com_winId_t iId, com_winpos_t *iMove )
+{
+    com_winpos_t  pos;
+    com_getWindowCur( iId, &pos );
+    pos.x = pos.x + iMove->x;
+    if( pos.x < 0 ) { pos.x = 0; }
+    pos.y = pos.y + iMove->y;
+    if( pos.y < 0 ) { pos.y = 0; }
+    com_moveCursor( iId, &pos );
+}
+
+static void cursorUp( com_winId_t iId )
+{
+    moveCursor( iId, &(com_winpos_t){ 0, -1 } );
+}
+
+static void cursorDown( com_winId_t iId )
+{
+    moveCursor( iId, &(com_winpos_t){ 0, 1 } );
+}
+
+static void cursorRight( com_winId_t iId )
+{
+    moveCursor( iId, &(com_winpos_t){ 1, 0 } );
+}
+
+static void cursorLeft( com_winId_t iId )
+{
+    moveCursor( iId, &(com_winpos_t){ -1, 0 } );
+}
+
+static void moveWindow( com_winId_t iId, com_winpos_t *iMove )
+{
+    com_winpos_t  pos;
+    com_getWindowPos( iId, &pos );
+    pos.x = pos.x + iMove->x;
+    pos.y = pos.y + iMove->y;
+    com_moveWindow( iId, &pos );
+}
+
+static void moveUpWindow( com_winId_t iId )
+{
+    moveWindow( iId, &(com_winpos_t){ 0, -1 } );
+}
+
+static void moveDownWindow( com_winId_t iId )
+{
+    moveWindow( iId, &(com_winpos_t){ 0, 1 } );
+}
+
+static void moveRightWindow( com_winId_t iId )
+{
+    moveWindow( iId, &(com_winpos_t){ 1, 0 } );
+}
+
+static void moveLeftWindow( com_winId_t iId )
+{
+    moveWindow( iId, &(com_winpos_t){ -1, 0 } );
+}
+
+static com_keymap_t  gBaseKeymap[] = {
+    { '\t',        switchWindow },
+    { 0x5a,        revertWindow },      // SHIFT+TAB っぽい
+    { 'k',         cursorUp },
+    { 'j',         cursorDown },
+    { 'l',         cursorRight },
+    { 'h',         cursorLeft },
+    {   0, NULL }
+};
+
+static com_keymap_t  gWinKeymap[] = {
+    { 0x41,         moveUpWindow },     // カーソル上
+    { 0x42,         moveDownWindow },   // カーソル下
+    { 0x43,         moveRightWindow },  // カーソル右
+    { 0x44,         moveLeftWindow },   // カーソル左
+    {   0, NULL }
+};
+
+static com_winId_t setupWin(
+        com_winpos_t *iPos, com_winpos_t *iSize, BOOL iBorder )
+{
+    com_winId_t id = com_createWindow( iPos, iSize, iBorder );
+    if( !id ) { return 0; }
+
+    com_mprintWindow( id, &(com_winpos_t){0,10}, A_REVERSE, true, "ID%ld", id );
+    if( !com_setWindowKeymap( id, gWinKeymap ) ) { return 0; }
+    if( !com_mixWindowKeymap( id, 0 ) ) { return 0; }
+    com_activateWindow( id );
+    gCurWin = id;
+    return id;
+}
+
+#define ABORT  gLoop=false;return;
+
+static void startWindow( void )
+{
+    com_winopt_t  winOpt = { true, true, false, NULL };
+    com_winpos_t  winSize;
+    if( !com_readyWindow( &winOpt, &winSize ) ) { ABORT; }
+    if( !com_setWindowKeymap( 0, gBaseKeymap ) ) { ABORT; }
+    com_mprintWindow( 0, &(com_winpos_t){0,winSize.y-1}, A_REVERSE, true,
+                      "BOTTOM" );
+    com_mprintWindow( 0, &(com_winpos_t){65,winSize.y-1}, A_UNDERLINE, true,
+                      "CTRL+C to quit" );
+
+    com_winpos_t  winPos  = { 10,  0 };
+    winSize = (com_winpos_t){ 40, 10 };
+    for( int id = 0; id < WIN_MAX; id++ ) {
+        if( !(gWinId[id] = setupWin( &winPos, &winSize, true )) ) { ABORT; }
+        winPos.x += 5;
+        winPos.y += 3;
+    }
+}
+
+static void quitLoop( int iSignal )
+{
+    // CTRL+C のみ終了の処理とする
+    if( iSignal == SIGINT ) { gLoop = false; }
+}
+
+static com_sigact_t gSigHandler[] = {
+    { SIGINT,         { .sa_handler = quitLoop } },
+    { SIGTSTP,        { .sa_handler = quitLoop } },
+    { COM_SIGACT_END, { .sa_handler = NULL } }
+};
+      
+static void loopMain( void ) 
+{
+    if( !com_setSignalAction( gSigHandler ) ) {return;}
+    gCurWin = 1;
+    while( gLoop ) {
+        com_activateWindow( gCurWin );
+        com_checkWindowKey( gCurWin, 50 );
+        int lastKey = com_getLastKey();
+        if( lastKey > 0 ) {
+            com_mprintWindow(
+                    0, &(com_winpos_t){0,0}, A_BOLD, true, "%02x", lastKey );
+        }
+    }
+}
+
+static void finishWindow( void )
+{
+    com_finishWindow();
+}
+
+/* 初期化処理 ****************************************************************/
+
+static void initialize( int iArgc, char **iArgv )
+{
+    COM_INITIALIZE( iArgc, iArgv );
+    // これ以降に他モジュールの初期化処理が続く想定
+}
+
+/* 起動オプション判定 *******************************************************/
+// 新しいオプションを追加したい時は、オプション処理関数を作成後、
+// gOptions[]に定義を追加する。詳細は com_if.hの com_getOption()の説明を参照。
+
+static BOOL viewVersion( com_getOptInf_t *iOptInf )
+{
+    COM_UNUSED( iOptInf );
+    // 特に何もせずに処理終了させる
+    com_exit( COM_NO_ERROR );
+    return true;
+}
+
+static BOOL clearLogs( com_getOptInf_t *iOptInf )
+{
+    COM_UNUSED( iOptInf );
+    com_system( "rm -fr .%s.log_*", com_getAplName() );
+    com_printf( "<<< cleared all past logs >>>\n" );
+    com_exit( COM_NO_ERROR );
+    return true;
+}
+
+#ifdef    USE_TESTFUNC    // テスト関数使用のコンパイルオプション
+static BOOL execTest( com_getOptInf_t *iOptInf )
+{
+    com_testCode( iOptInf->argc, iOptInf->argv );
+    return true;
+}
+#endif // USE_TESTFUNC
+
+static com_getOpt_t gOptions[] = {
+    { 'v', "version",  0,           0, false, viewVersion },
+    { 'c', "clearlog", 0,           0, false, clearLogs },
+#ifdef    USE_TESTFUNC
+    {   0, "debug",    COM_OPT_ALL, 0, false, execTest },
+#endif // USE_TESTFUNC
+    COM_OPTLIST_END
+};
+
+/* メイン関数 ***************************************************************/
+
+int main( int iArgc, char **iArgv )
+{
+    initialize( iArgc, iArgv );
+    long   restArgc = 0;    // オプションチェック後、まだ残ったオプション数
+    char** restArgv = NULL; // オプションチェック後の内容リスト
+    com_getOption( iArgc, iArgv, gOptions, &restArgc, &restArgv, NULL );
+
+    /* 以後、必要な処理を記述 */
+    startWindow();
+    loopMain();
+    finishWindow();
+
+    return EXIT_SUCCESS;
+}
+
